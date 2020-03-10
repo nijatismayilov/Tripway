e8911cf99064167095f360751ab59d86cc735d05
nicat_ismayilov <nicatismayilov43@gmail.com>
2020-03-10 14:26:57 +0400

    small changes to slider source code, JS backticks implemented

--- scripts/slider.js
+++ scripts/slider.js
@@ -8,32 +8,32 @@ let counter = 1;
 
 const size = sliderItems[0].clientWidth;
 
-slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+slider.style.transform = `translateX(${ -size * counter }px)`;
 
 btnNext.addEventListener('click', () => {
     if (counter >= (sliderItems.length - 1)) return;
     slider.style.transition = 'transform 0.4s ease-in-out';
     counter++;
-    slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    slider.style.transform = `translateX(${ -size * counter }px)`;
 });
 
 btnPrev.addEventListener('click', () => {
     if (counter <= 0) return;
     slider.style.transition = 'transform 0.4s ease-in-out';
     counter--;
-    slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    slider.style.transform = `translateX(${ -size * counter }px)`;
 });
 
 slider.addEventListener('transitionend', () => {
     if (sliderItems[counter].getAttribute('id') === 'last-copy') {
         slider.style.transition = 'none';
         counter = sliderItems.length - 2;
-        slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+        slider.style.transform = `translateX(${ -size * counter }px)`;
     }
 
     if (sliderItems[counter].getAttribute('id') === 'first-copy') {
         slider.style.transition = 'none';
         counter = sliderItems.length - counter;
-        slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+        slider.style.transform = `translateX(${ -size * counter }px)`;
     }
 });
